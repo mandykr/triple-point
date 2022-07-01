211a03fe1c77772a920b959c3f9ed0d48fe5e720
mandykr <hongseokii92@gmail.com>
2022-07-01 21:27:41 +0900

    test: 리뷰 생성에 대한 포인트 적립 테스트 수정 - 픽스처 메소드 활용

--- point-service/src/test/java/com/triple/pointservice/domain/PointAddCalculatorTest.java
+++ point-service/src/test/java/com/triple/pointservice/domain/PointAddCalculatorTest.java
@@ -1,6 +1,5 @@
 package com.triple.pointservice.domain;
 
-import com.triple.pointservice.domain.event.PointEvent;
 import com.triple.pointservice.domain.event.PointEvents;
 import com.triple.pointservice.domain.event.ReviewEventAction;
 import com.triple.pointservice.domain.exception.PointEventAlreadySavedException;
@@ -10,28 +9,33 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 
 import static com.triple.pointservice.domain.ReviewFixture.*;
 import static com.triple.pointservice.domain.event.PointEventAction.ADD;
 import static com.triple.pointservice.domain.event.PointEventAction.DELETE;
-import static com.triple.pointservice.domain.event.PointEventType.ADDED_FIRST_REVIEW_ON_PLACE;
+import static com.triple.pointservice.domain.event.PointEventFixture.createEvent;
+import static com.triple.pointservice.domain.event.PointEventFixture.createTextEvent;
+import static com.triple.pointservice.domain.event.PointEventType.*;
+import static com.triple.pointservice.domain.event.PointEventsFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-@DisplayName("리뷰 추가에 대한 포인트를 계산한다")
+@DisplayName("리뷰 추가에 대한 포인트를 계산해 이벤트를 생성한다")
 class PointAddCalculatorTest {
     private PointCalculator pointCalculator;
     private PointPolicy pointPolicy;
-    private PointEvents savePlaceEvents;
+    private PointEvents savedPlaceEvents;
 
     @BeforeEach
     void setUp() {
         pointCalculator = new PointAddCalculator();
         pointPolicy = new DefaultPointPolicy();
-        savePlaceEvents = new PointEvents(List.of(
-                new PointEvent(ADDED_FIRST_REVIEW_ON_PLACE, ADD, REVIEW_ID, USER_ID, PLACE_ID, 1))
+        savedPlaceEvents = new PointEvents(List.of(
+                createEvent(ADDED_FIRST_REVIEW_ON_PLACE, ADD, pointPolicy, LocalDate.now()))
         );
     }
 
@@ -42,11 +46,14 @@ class PointAddCalculatorTest {
         Review review = createTextReview(ReviewEventAction.ADD);
 
         // when
-        PointEvents events = pointCalculator.calculate(review, pointPolicy, new PointEvents(), savePlaceEvents);
+        PointEvents events = pointCalculator.calculate(review, pointPolicy, new PointEvents(), savedPlaceEvents);
 
-        // given
-        int size = events.getPointEvents().size();
-        assertThat(size).isEqualTo(1);
+        // then
+        assertAll(
+                () -> assertThat(getSize(events)).isEqualTo(1),
+                () -> assertThat(getTypes(events)).containsOnly(WRITE_TEXT),
+                () -> assertThat(getActions(events)).containsOnly(ADD)
+        );
     }
 
     @DisplayName("리뷰에 텍스트가 비어있으면 텍스트 작성 이벤트를 추가하지 않는다")
@@ -57,11 +64,10 @@ class PointAddCalculatorTest {
         Review review = createReview(ReviewEventAction.ADD, text, Collections.emptyList());
 
         // when
-        PointEvents events = pointCalculator.calculate(review, pointPolicy, new PointEvents(), savePlaceEvents);
+        PointEvents events = pointCalculator.calculate(review, pointPolicy, new PointEvents(), savedPlaceEvents);
 
-        // given
-        int size = events.getPointEvents().size();
-        assertThat(size).isZero();
+        // then
+        assertThat(getSize(events)).isZero();
     }
 
     @DisplayName("리뷰에 1장 이상의 사진이 첨부되어 있으면 사진 첨부 이벤트를 추가한다")
@@ -71,11 +77,14 @@ class PointAddCalculatorTest {
         Review review = createPhotoReview(ReviewEventAction.ADD);
 
         // when
-        PointEvents events = pointCalculator.calculate(review, pointPolicy, new PointEvents(), savePlaceEvents);
+        PointEvents events = pointCalculator.calculate(review, pointPolicy, new PointEvents(), savedPlaceEvents);
 
-        // given
-        int size = events.getPointEvents().size();
-        assertThat(size).isEqualTo(1);
+        // then
+        assertAll(
+                () -> assertThat(getSize(events)).isEqualTo(1),
+                () -> assertThat(getTypes(events)).containsOnly(ATTACHED_PHOTOS),
+                () -> assertThat(getActions(events)).containsOnly(ADD)
+        );
     }
 
     @DisplayName("리뷰에 사진이 첨부되어 있지 않으면 사진 첨부 이벤트를 추가하지 않는다")
@@ -85,11 +94,10 @@ class PointAddCalculatorTest {
         Review review = createReview(ReviewEventAction.ADD, "", Collections.emptyList());
 
         // when
-        PointEvents events = pointCalculator.calculate(review, pointPolicy, new PointEvents(), savePlaceEvents);
+        PointEvents events = pointCalculator.calculate(review, pointPolicy, new PointEvents(), savedPlaceEvents);
 
-        // given
-        int size = events.getPointEvents().size();
-        assertThat(size).isZero();
+        // then
+        assertThat(getSize(events)).isZero();
     }
 
     @DisplayName("리뷰에 텍스트와 사진이 등록되어 있으면 텍스트 작성 이벤트와 사진 첨부 이벤트를 추가한다")
@@ -99,42 +107,48 @@ class PointAddCalculatorTest {
         Review review = createReview(ReviewEventAction.ADD, CONTENT, ATTACHED_PHOTO_IDS);
 
         // when
-        PointEvents events = pointCalculator.calculate(review, pointPolicy, new PointEvents(), savePlaceEvents);
+        PointEvents events = pointCalculator.calculate(review, pointPolicy, new PointEvents(), savedPlaceEvents);
 
-        // given
-        int size = events.getPointEvents().size();
-        assertThat(size).isEqualTo(2);
+        // then
+        assertAll(
+                () -> assertThat(getSize(events)).isEqualTo(2),
+                () -> assertThat(getTypes(events)).containsOnly(WRITE_TEXT, ATTACHED_PHOTOS),
+                () -> assertThat(getActions(events)).containsOnly(ADD)
+        );
     }
 
-    @DisplayName("리뷰 추가에 대한 이벤트가 모두 삭제되어 있으면 이벤트가 정상 추가된다")
+    @DisplayName("리뷰에 대한 이벤트가 모두 삭제되어 있으면 이벤트가 정상 추가된다")
     @Test
     void alreadySavedAndDeleted() {
         // given
         Review review = createReview(ReviewEventAction.ADD, CONTENT, ATTACHED_PHOTO_IDS);
-        PointEvents saveEvents = new PointEvents(List.of(
-                new PointEvent(ADDED_FIRST_REVIEW_ON_PLACE, DELETE, REVIEW_ID, USER_ID, PLACE_ID, 1))
+        PointEvents savedEvents = createPointEvents(
+                createTextEvent(DELETE, pointPolicy, LocalDate.now())
         );
 
         // when
-        PointEvents events = pointCalculator.calculate(review, pointPolicy, saveEvents, savePlaceEvents);
+        PointEvents events = pointCalculator.calculate(review, pointPolicy, savedEvents, savedPlaceEvents);
 
-        // given
-        int size = events.getPointEvents().size();
-        assertThat(size).isEqualTo(2);
+        // then
+        assertAll(
+                () -> assertThat(getSize(events)).isEqualTo(2),
+                () -> assertThat(getTypes(events)).containsOnly(WRITE_TEXT, ATTACHED_PHOTOS),
+                () -> assertThat(getActions(events)).containsOnly(ADD)
+        );
     }
 
-    @DisplayName("이미 리뷰 추가에 대한 이벤트가 등록되어 있으면 PointEventAlreadySavedException이 발생한다")
+    @DisplayName("이미 리뷰에 대한 이벤트가 등록되어 있으면 예외가 발생한다")
     @Test
     void alreadySavedContentReview() {
         // given
         Review review = createTextReview(ReviewEventAction.ADD);
-        PointEvents saveEvents = new PointEvents(List.of(
-                new PointEvent(ADDED_FIRST_REVIEW_ON_PLACE, ADD, REVIEW_ID, USER_ID, PLACE_ID, 1))
+        PointEvents savedEvents = createPointEvents(
+                createTextEvent(ADD, pointPolicy, LocalDate.now())
         );
 
         // when, then
         assertThatThrownBy(() -> {
-            pointCalculator.calculate(review, pointPolicy, saveEvents, new PointEvents());
+            pointCalculator.calculate(review, pointPolicy, savedEvents, new PointEvents());
         }).isInstanceOf(PointEventAlreadySavedException.class);
     }
 
@@ -147,9 +161,12 @@ class PointAddCalculatorTest {
         // when
         PointEvents events = pointCalculator.calculate(review, pointPolicy, new PointEvents(), new PointEvents());
 
-        // given
-        int size = events.getPointEvents().size();
-        assertThat(size).isEqualTo(2);
+        // then
+        assertAll(
+                () -> assertThat(getSize(events)).isEqualTo(2),
+                () -> assertThat(getTypes(events)).containsOnly(WRITE_TEXT, ADDED_FIRST_REVIEW_ON_PLACE),
+                () -> assertThat(getActions(events)).containsOnly(ADD)
+        );
     }
 
     @DisplayName("장소에 처음 등록하는 포토 리뷰이면 사진 첨부, 첫 리뷰 이벤트를 추가한다")
@@ -161,9 +178,12 @@ class PointAddCalculatorTest {
         // when
         PointEvents events = pointCalculator.calculate(review, pointPolicy, new PointEvents(), new PointEvents());
 
-        // given
-        int size = events.getPointEvents().size();
-        assertThat(size).isEqualTo(2);
+        // then
+        assertAll(
+                () -> assertThat(getSize(events)).isEqualTo(2),
+                () -> assertThat(getTypes(events)).containsOnly(ATTACHED_PHOTOS, ADDED_FIRST_REVIEW_ON_PLACE),
+                () -> assertThat(getActions(events)).containsOnly(ADD)
+        );
     }
 
     @DisplayName("장소에 처음 등록하는 텍스트, 포토 리뷰이면 텍스트 작성, 사진 첨부, 첫 리뷰 이벤트를 추가한다")
@@ -175,9 +195,13 @@ class PointAddCalculatorTest {
         // when
         PointEvents events = pointCalculator.calculate(review, pointPolicy, new PointEvents(), new PointEvents());
 
-        // given
-        int size = events.getPointEvents().size();
-        assertThat(size).isEqualTo(3);
+        // then
+        assertAll(
+                () -> assertThat(getSize(events)).isEqualTo(3),
+                () -> assertThat(getTypes(events))
+                        .containsOnly(WRITE_TEXT, ATTACHED_PHOTOS, ADDED_FIRST_REVIEW_ON_PLACE),
+                () -> assertThat(getActions(events)).containsOnly(ADD)
+        );
     }
 
     @DisplayName("장소에 이미 리뷰가 등록되어 있으면 첫 리뷰 이벤트를 추가하지 않는다")
@@ -187,10 +211,13 @@ class PointAddCalculatorTest {
         Review review = createReview(ReviewEventAction.ADD, CONTENT, ATTACHED_PHOTO_IDS);
 
         // when
-        PointEvents events = pointCalculator.calculate(review, pointPolicy, new PointEvents(), savePlaceEvents);
+        PointEvents events = pointCalculator.calculate(review, pointPolicy, new PointEvents(), savedPlaceEvents);
 
-        // given
-        int size = events.getPointEvents().size();
-        assertThat(size).isEqualTo(2);
+        // then
+        assertAll(
+                () -> assertThat(getSize(events)).isEqualTo(2),
+                () -> assertThat(getTypes(events)).containsOnly(WRITE_TEXT, ATTACHED_PHOTOS),
+                () -> assertThat(getActions(events)).containsOnly(ADD)
+        );
     }
 }
