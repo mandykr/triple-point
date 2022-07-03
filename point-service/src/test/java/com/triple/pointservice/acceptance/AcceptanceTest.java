827bd7370a3332c1b6412cafefb9dd89bdddb8e0
mandykr <hongseokii92@gmail.com>
2022-07-04 07:48:04 +0900

    test: 인수테스트 DB 초기화 로직 추가

--- point-service/src/test/java/com/triple/pointservice/acceptance/AcceptanceTest.java
+++ point-service/src/test/java/com/triple/pointservice/acceptance/AcceptanceTest.java
@@ -1,5 +1,6 @@
 package com.triple.pointservice.acceptance;
 
+import com.triple.pointservice.utils.DatabaseCleanup;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +14,12 @@ public class AcceptanceTest {
     @Autowired
     Environment environment;
 
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
     @BeforeEach
     public void setUp() {
         RestAssured.port = Integer.parseInt(Objects.requireNonNull(environment.getProperty("local.server.port")));
+        databaseCleanup.execute();
     }
 }
